94896e18462aaf8cab15a9b792f3843149268a98
pankaj bansal <bansalpankaj89@gmail.com>
2017-09-26 19:35:31 +0800

    clean code

--- www/app/app.js
+++ www/app/app.js
@@ -1,4 +1,4 @@
-var app=angular.module("myApp", ['ngMaterial','ngMdIcons','ngCordova','cgBusy','ui.router']);
+var app=angular.module("myApp", ['ngMaterial','ngMdIcons','cgBusy','ui.router']);
 
 app.config(["$stateProvider", "$urlRouterProvider", "$mdThemingProvider", "$httpProvider",
     function($stateProvider, $urlRouterProvider, $mdThemingProvider,$httpProvider) {
